4bd8600e06292b34e39bbaf62e3ebd2dc9bc8522
David Botero <davisa200404@gmail.com>
2026-09-12 11:15:52 -0500

    Respond to GET / with HTML (exercise 1.5)

--- the_project/index.js
+++ the_project/index.js
@@ -3,8 +3,8 @@ const http = require('http')
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end(`Server started in port ${PORT}`)
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end('<h1>Todo App</h1><p>The project is up and running.</p>')
 })
 
 server.listen(PORT, () => {
